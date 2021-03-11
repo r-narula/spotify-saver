7c273d9423512a28d946463ebcaff6b90540a84f
r_narula <aarav30302001@gmail.com>
2021-03-12 01:12:59 +0530

    added path variable

--- spotify.bash
+++ spotify.bash
@@ -1,12 +1,16 @@
 
 # makes a data field
 dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.freedesktop.DBus.Properties.Get string:org.mpris.MediaPlayer2.Player string:Metadata >>/home/$USER/spotify-saver/spotify_music.txt
+path="/home/$USER/spotify-saver"
+text="spotify_music.txt"
+text_path="$path/$text"
+
 
 # extracts the data
 # makes the array of targets in which we have the info stored.. 
 IFS_backup=$IFS
 IFS=$'\n'
-targets=($(grep -o '".*"' /home/$USER/spotify-saver/spotify_music.txt | sed 's/"//g')) # grep then replace every " with NULL
+targets=($(grep -o '".*"' $text_path | sed 's/"//g')) # grep then replace every " with NULL
 IFS=$IFS_backup
 # clear the file contents for next time use
 # send the specified fields to array .txt as csv values
@@ -16,4 +20,5 @@ c=${targets[17]} # song url
 final="${a},${b},${c}"
 echo Saved $a
 echo $final >> /home/$USER/spotify-saver/spotify_music.csv
-> /home/$USER/spotify-saver/spotify_music.txt
+> $text_path
+
